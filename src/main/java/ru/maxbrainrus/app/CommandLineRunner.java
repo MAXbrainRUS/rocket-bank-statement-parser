fe01ab341c3123bbf580bc288d63186187f2bd60
MAXbrainRUS <MAXbrainRUS@gmail.com>
2019-03-06 23:08:19 +0300

    move command runner to picocli

--- src/main/java/ru/maxbrainrus/app/CommandLineRunner.java
+++ src/main/java/ru/maxbrainrus/app/CommandLineRunner.java
@@ -1,79 +1,48 @@
 package ru.maxbrainrus.app;
 
-import org.kohsuke.args4j.Argument;
-import org.kohsuke.args4j.CmdLineException;
-import org.kohsuke.args4j.CmdLineParser;
-import org.kohsuke.args4j.Option;
+import picocli.CommandLine;
 import ru.maxbrainrus.parser.RocketParserController;
 
 import java.io.File;
-import java.io.PrintStream;
-import java.util.*;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
 
-public class CommandLineRunner {
+@CommandLine.Command(name = "rocketParser", mixinStandardHelpOptions = true, version = "1.0")
+public class CommandLineRunner implements Runnable {
     public static final String DEFAULT_KEY_WORDS_TO_CATEGORY_MAP_JSON_PATH = "KeyWordsToCategoryMap.json";
     public static final String customCategoryMapOptionDescription = "Specific path to json file with map {\"key word\":\"category\"} for auto filling of category for transactions. By default try to use " + DEFAULT_KEY_WORDS_TO_CATEGORY_MAP_JSON_PATH + " in the app working directory";
-    @Option(name = "--help", usage = "Show help", help = true)
-    private boolean help;
 
-    @Option(name = "-m", aliases = "--custom-category-map", usage = customCategoryMapOptionDescription, help = true)
+    @CommandLine.Option(names = {"-m", "--custom-category-map"},
+            description = customCategoryMapOptionDescription,
+            defaultValue = DEFAULT_KEY_WORDS_TO_CATEGORY_MAP_JSON_PATH)
     private File pathToKeyWordToMapConfig;
 
-    @Argument
-    private List<String> arguments = new ArrayList<String>();
+    @CommandLine.Parameters(index = "0", descriptionKey = "source.pdf", description = "rocket statement pdf file")
+    private String sourceStatementFilename;
+
+    @CommandLine.Parameters(index = "1", descriptionKey = "report.csv", description = "result report filename",
+            defaultValue = "report.csv")
+    private String reportFilename;
+
 
     public static void main(String[] args) {
-        new CommandLineRunner().doMain(args);
+        CommandLine.run(new CommandLineRunner(), args);
     }
 
-    private void doMain(String[] args) {
-        CmdLineParser parser = new CmdLineParser(this);
-
-        try {
-            parser.parseArgument(args);
-
-            if (help) {
-                printUsage(parser, System.out);
-                return;
-            }
-
-            if (arguments.isEmpty()) {
-                throw new CmdLineException(parser, "No argument is given", new IllegalStateException());
-            }
-
-            if (arguments.size() > 2) {
-                throw new CmdLineException(parser, "Too many arguments. Should be one or two", new IllegalStateException());
-            }
-        } catch (CmdLineException e) {
-            System.err.println(e.getMessage());
-            printUsage(parser, System.err);
-            System.err.println();
-            return;
-        }
-
-        String inputDataFileName = arguments.get(0);
-        String resultFileName = arguments.size() >= 2 ? arguments.get(1) : "report.xls";
+    @Override
+    public void run() {
         Map<String, String> keyWordsToCategoryMap = getConfigFileMapKeyWordsToCategory()
                 .map(KeyWordsToCategoryMapJsonParser::parseConfigJson)
                 .orElse(Collections.emptyMap());
 
-        RocketParserController.makeReport(inputDataFileName, resultFileName, keyWordsToCategoryMap);
+        RocketParserController.makeReport(sourceStatementFilename, reportFilename, keyWordsToCategoryMap);
     }
 
     private Optional<File> getConfigFileMapKeyWordsToCategory() {
-        if (pathToKeyWordToMapConfig == null) {
-            File config = new File(DEFAULT_KEY_WORDS_TO_CATEGORY_MAP_JSON_PATH);
-            if (config.isFile() && config.exists()) {
-                return Optional.of(config);
-            }
-            return Optional.empty();
+        if (pathToKeyWordToMapConfig.isFile() && pathToKeyWordToMapConfig.exists()) {
+            return Optional.of(pathToKeyWordToMapConfig);
         }
-        return Optional.of(pathToKeyWordToMapConfig);
-    }
-
-    private void printUsage(CmdLineParser parser, PrintStream printStream) {
-        printStream.println("usage: rocketParser [options] source.pdf [result.xls]");
-        printStream.println("Options:");
-        parser.printUsage(printStream);
+        return Optional.empty();
     }
 }
